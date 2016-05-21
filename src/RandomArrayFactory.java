c81e5cf3f0251703d8046d8a62659afc991b3a95
Yair M <yairm@theneura.com>
2016-05-21 11:20:54 +0300

    added javadoc for getRandomArray

--- src/RandomArrayFactory.java
+++ src/RandomArrayFactory.java
@@ -1,21 +1,25 @@
 
 public class RandomArrayFactory {
-	
-	
-	
-	
-	public static double[] getRandomArray(int size){
-		
-		//first create the array
+
+	/**
+	 * A function to generate a random array of doubles
+	 * 
+	 * @param size
+	 *            - the size of the array to generate
+	 * @return - a random in size 'size' with random doubles rounded up to
+	 *         10digits
+	 */
+	public static double[] getRandomArray(int size) {
+
+		// first create the array
 		double[] RandomArray = new double[size];
-		//fill the array with random doubles using math.random
+		// fill the array with random doubles using math.random
 		for (int i = 0; i < RandomArray.length; i++) {
-			//generate a random number, round it to have 10 digits.
-			RandomArray[i] = (double) Math.round(Math.random() * 10000000000d) / 10000000000d ;
+			// generate a random number, round it to have 10 digits.
+			RandomArray[i] = (double) Math.round(Math.random() * 10000000000d) / 10000000000d;
 		}
-		
+
 		return RandomArray;
-		
-		
+
 	}
 }
---
+++ src/tester.java
@@ -0,0 +1,16 @@
+
+public class tester {
+
+	public static void main(String[] args) {
+		
+		
+		 double[] randfact = RandomArrayFactory.getRandomArray(6);
+		
+		for (int i = 0; i < 6; i++) {
+			System.out.println(randfact[i]);
+			
+		}
+
+	}
+
+}
